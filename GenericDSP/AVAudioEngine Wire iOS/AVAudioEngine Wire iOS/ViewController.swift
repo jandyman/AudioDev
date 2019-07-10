a05bfa6c6248999d4b9021bb8143a029eaf1b775
Andrew Voelkel <avoelkel@setpointmedical.conm>
2019-07-10 10:12:45 -0700

    4 channel wire demo working WITH audio unit!

--- GenericDSP/AVAudioEngine Wire iOS/AVAudioEngine Wire iOS/ViewController.swift
+++ GenericDSP/AVAudioEngine Wire iOS/AVAudioEngine Wire iOS/ViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 import AVFoundation
 
+extension OSType {
+  init(_ string: String) {
+    let utf8 = string.utf8
+    precondition(utf8.count == 4, "Must be a 4 char string")
+    var out: UInt32 = 0
+    for char in utf8 {
+      out <<= 8
+      out |= UInt32(char)
+    }
+    self = out
+  }
+}
+
 class ViewController: UIViewController {
   
   var session = AVAudioSession.sharedInstance()
   var engine = AVAudioEngine()
+  var audioUnit : AVAudioUnit?
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    createAU()
     
     let nc = NotificationCenter.default
     nc.addObserver(self,
@@ -32,18 +48,39 @@ class ViewController: UIViewController {
     
   }
   
+  func createAU() {
+    let type = OSType("aufx")
+    let subtype = OSType("thru")
+    let manufacturer = OSType("Demo")
+    let desc = AudioComponentDescription(componentType: type,
+                                         componentSubType: subtype,
+                                         componentManufacturer: manufacturer,
+                                         componentFlags: 0, componentFlagsMask: 0)
+    AUAudioUnit.registerSubclass(ThruAudioUnit.self, as: desc, name: "Wire AU demo", version: UInt32.max)
+    AVAudioUnit.instantiate(with: desc, options: .loadOutOfProcess, completionHandler: auInstantiated)
+  }
+  
+  private func auInstantiated(au: AVAudioUnit?, err: Error?) {
+    audioUnit = au!
+  }
+
+  
   @objc func handleRouteChange(notification: Notification) {
     let inputs = session.currentRoute.inputs
     assert(inputs.count == 1)
     let nChannels = inputs[0].channels?.count
     
     do {
-      if nChannels == 4 {
+      if nChannels == 4 && audioUnit != nil {
         print("starting engine")
         // create 4 channel layout
         let layout = AVAudioChannelLayout(layoutTag: kAudioChannelLayoutTag_Unknown | 4)!
+//        let layout = AVAudioChannelLayout(layoutTag: kAudioChannelLayoutTag_Stereo)!
         let audioFormat = AVAudioFormat(standardFormatWithSampleRate: 44100, channelLayout: layout)
-        engine.connect(engine.inputNode, to: engine.outputNode, format: audioFormat)
+//        let audioFormat = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 2)
+        engine.attach(audioUnit!)
+        engine.connect(engine.inputNode, to: audioUnit!, format: audioFormat)
+        engine.connect(audioUnit!, to: engine.outputNode, format: audioFormat)
         try engine.start()
       } else {
         engine.stop()
