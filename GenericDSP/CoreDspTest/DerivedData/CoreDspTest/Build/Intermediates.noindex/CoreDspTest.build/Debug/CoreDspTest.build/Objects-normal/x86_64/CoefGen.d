b408308bf1b3d94dd181c6aa1ea4ab1cb925706e
Andy Voelkel <avoelkel@setpointmedical.com>
2020-02-01 19:38:27 -0700

    Don't save DerivedData

--- GenericDSP/CoreDspTest/DerivedData/CoreDspTest/Build/Intermediates.noindex/CoreDspTest.build/Debug/CoreDspTest.build/Objects-normal/x86_64/CoefGen.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/andy/Dropbox\ (Personal)/Developer/AudioDev/GenericDSP/Common/DSP/CoefGen.cpp \
-  /Users/andy/Dropbox\ (Personal)/Developer/AudioDev/GenericDSP/Common/DSP/CoefGen.hpp
